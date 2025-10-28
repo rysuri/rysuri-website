063ca34760c83129c4890e88a8bed432d076469d
Ry Suriyathep <rysu986@gmail.com>
2025-10-27 18:49:15 -0700

    i hate everything

--- front/src/pages/Contact.jsx
+++ front/src/pages/Contact.jsx
@@ -21,7 +21,7 @@ function Contact() {
     setLoading(true);
 
     try {
-      const res = await fetch(`${import.meta.env.VITE_API_URL}contact`, {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/contact`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
